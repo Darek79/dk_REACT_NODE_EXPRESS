6f62d82a825a105c794a41302f8f53ed1162fd04
unknown <buzzer79@gmail.com>
2019-07-05 02:51:57 +0200

    oauth

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-// require('dotenv').config()
+require('dotenv').config()
 const express = require('express');
+const passport = require('passport');
+const {
+    Strategy
+} = require('passport-google-oauth20');
+
 const app = express();
+// app.use(express.urlencoded({
+//     extended: false
+// }));
+// app.use(express.json());
 
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+passport.use(new Strategy({
+    clientID: process.env.clientID,
+    clientSecret: process.env.clientKey,
+    callbackURL: "/auth/google/callback"
+}, (accessToken) => {
+    console.log(accessToken);
+}));
 
-app.get("*",(req,res)=>{
-    res.json({
-        message:"here iam"
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile','email']
     })
-});
+    );
 
-const PORT = process.env.PORT || 3000;
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/login'
+    }),
+    (req, res)=> {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
